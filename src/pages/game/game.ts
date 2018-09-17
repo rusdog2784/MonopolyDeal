700073aa618aaf4a70be4888c20b86b912db43ae
Scott Russell <scott.tkdmaster@mac.com>
2018-09-17 18:48:11 -0400

    Commentted out game.html so that I could work with the server. As of now, players can enter and be notified of other players entering. The first player to enter creates the deck (might want to change it to the last person in). One deck is sent and shared between players.

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController, PopoverController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
 import { Deck } from '../../app/models/Deck';
 import { Card } from '../../app/models/Card';
 import { Player } from '../../app/models/Player';
@@ -7,6 +8,7 @@ import { CardPopover } from '../card_popover/card_popover';
 import { Socket } from 'ng-socket-io';
 import { PropertyCard } from '../../app/models/PropertyCard';
 import { PropertyType } from '../../app/models/PropertyType';
+import { Observable } from '../../../node_modules/rxjs/Observable';
 
 @Component({
   selector: 'page-game',
@@ -14,51 +16,78 @@ import { PropertyType } from '../../app/models/PropertyType';
 })
 export class GamePage {
     deck: Deck;
+    mainPlayer: Player;
     playedCards: Card[];
-    players: Player[];
+    opponents: Player[];
 
-    constructor(public navCtrl: NavController, public alertCtrl: AlertController, public popoverCtrl: PopoverController, private socket: Socket) {
-        this.players = [];
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController, public popoverCtrl: PopoverController, private socket: Socket, private navParams: NavParams) {
+        this.opponents = [];
+        this.mainPlayer = this.navParams.get('player');
         this.setupGame();
     }
 
     setupGame() {
+        this.subscribeToPlayers().subscribe(data => {
+            console.log("[game.ts] New player.");
+            console.log(data);
+            console.log(typeof(data));
+            let players: Player[] = data['players'];
+            for (let p of players) {
+                if (p.firstName != this.mainPlayer.firstName || p.lastName != this.mainPlayer.lastName) {
+                    if (this.opponents.indexOf(p) < 0) {
+                        console.log("[game.ts] Adding player to opponent list");
+                        console.log(p);
+                        this.opponents.push(p);
+                    }
+                }
+            }
+        });
+
+        this.subscribeToCreateNewDeck().subscribe(() => {
+            console.log("[game.ts] Asked to create a new deck!");
+            this.createDeck();
+        });
+
+        this.subscribeToDeck().subscribe(data => {
+            let updatedDeck: Deck = data['deck'];
+            console.log("[game.ts] Updated deck: ");
+            console.log(updatedDeck);
+            this.deck = updatedDeck;
+        });
+    }
+
+    subscribeToPlayers() {
+        let observable = new Observable(observer => {
+            this.socket.on('new-player', (data) => {
+                observer.next(data);
+            });
+        });
+        return observable;
+    }
+
+    subscribeToCreateNewDeck() {
+        let observable = new Observable(observer => {
+            this.socket.on('create-deck', (data) => {
+                observer.next(data);
+            });
+        });
+        return observable;
+    }
+
+    subscribeToDeck() {
+        let observable = new Observable(observer => {
+            this.socket.on('deck-change', (data) => {
+                observer.next(data);
+            });
+        });
+        return observable;
+    }
+
+    createDeck() {
         this.deck = new Deck();
-        this.playedCards = [new Card("Played Cards", 0)];
-
-        let player1 = new Player("Scott", "Russell", 0);
-        for (var i = 0; i < 5; i++) {
-            player1.hand.push(this.deck.cards[0]);
-            this.deck.cards.splice(0, 1);
-        }
-        /*
-        let northCarolinaAve = new PropertyCard("North Carolina Avenue", 4, PropertyType.Green);
-        let pacificAvenue = new PropertyCard("Pacific Avenue", 4, PropertyType.Green);
-        let pennsylvaniaAvenue = new PropertyCard("Pennsylvania Avenue", 4, PropertyType.Green);
-        let boardwalk = new PropertyCard("Boardwalk", 4, PropertyType.DarkBlue);
-        let parkPlace = new PropertyCard("Park Place", 4, PropertyType.DarkBlue);
-        player1.hand.push(northCarolinaAve);
-        player1.hand.push(pacificAvenue);
-        player1.hand.push(pennsylvaniaAvenue);
-        player1.hand.push(boardwalk);
-        player1.hand.push(parkPlace);
-        */
-        this.players.push(player1);
-        console.log("[game.ts] Player 1, " + player1.firstName + ", has been created.");
-        console.log("[game.ts] Their hand contains:");
-        console.log(player1.hand);
-
-        let player2 = new Player("Morgan", "Lynn", 0);
-        for (var i = 0; i < 5; i++) {
-            player2.hand.push(this.deck.cards[0]);
-            this.deck.cards.splice(0, 1);
-        }
-        this.players.push(player2);
-        console.log("[game.ts] Player 2, " + player2.firstName + ", has been created.");
-        console.log("[game.ts] Their hand contains:");
-        console.log(player2.hand);
-
-        console.log(this.players);
+        this.socket.emit('new-deck', {
+            deck: this.deck.cards
+        });
     }
 
     presentCardOptions(ev, card: Card) {
@@ -66,7 +95,7 @@ export class GamePage {
         console.log(card);
         let popover = this.popoverCtrl.create(CardPopover, {
             playedCards: this.playedCards,
-            player: this.players[0],
+            player: this.opponents[0],
             card: card,
             event: ev
         });
@@ -77,13 +106,13 @@ export class GamePage {
 
     pull2Cards() {
         for (var i = 0; i < 2; i++) {
-            this.players[0].hand.push(this.deck.cards[0]);
+            this.opponents[0].hand.push(this.deck.cards[0]);
             this.deck.cards.splice(0, 1);
         }
     }
 
     endTurn() {
-        if (this.players[0].hand.length > 7) {
+        if (this.opponents[0].hand.length > 7) {
             const alert = this.alertCtrl.create({
                 title: 'Whoops!',
                 subTitle: 'You seem to have too many cards. Please select cards you want to discard.',
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,17 +2,31 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GamePage } from '../game/game';
 import { DataProvider } from '../../providers/data/data';
+import { Socket } from 'ng-socket-io';
+import { Player } from '../../app/models/Player';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, public dataProvider: DataProvider) {
+  player: Player;
+
+  constructor(public navCtrl: NavController, public dataProvider: DataProvider, private socket: Socket) {
+    this.player = dataProvider.getPlayer();
+    console.log("Player: " + this.player.firstName + " " + this.player.lastName);
   }
 
   newGame(event, item) {
-    this.navCtrl.push(GamePage);
+    if (this.player.firstName == "" || this.player.lastName == "") {
+      console.log("Player name is empty");
+      return;
+    }
+    this.socket.connect();
+    this.socket.emit('player-entered', this.player);
+    this.navCtrl.push(GamePage, {
+      player: this.player
+    });
   }
 
 }
